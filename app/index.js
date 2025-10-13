66a1be86a0f49f74a7662247a33902e483efcce7
fergonalcantara <fergonalcantara@gmail.com>
2025-10-12 23:24:52 -0600

    Route default & server up

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+// Corregimos __dirname absoluta
+import path from 'path';
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Creamos nuestro Servidor
+const app = express();
+app.set("port", 4000);
+app.listen(app.get("port"));
+console.log("Servidor corriendo en el puerto ", app.get("port"));
+
+// Rutas definidas
+app.get("/", (req,res)=> res.sendFile(__dirname + "/pages/login.html"))
