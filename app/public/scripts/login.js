bbd16f3bee9754aa990ada73725e86cdd3ee49e7
fergonalcantara <fergonalcantara@gmail.com>
2025-10-14 12:48:07 -0600

    Login - login.js POST method

--- app/public/scripts/login.js
+++ app/public/scripts/login.js
@@ -1,1 +1,27 @@
-console.log("loginjs")
+const mensajeError = document.getElementsByClassName("error")[0];
+
+document.getElementById("login-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const body = {
+        user: form.elements.user.value,
+        password: form.elements.password.value
+    };
+
+    const res = await fetch("http://localhost:4000/api/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+
+    if(!res.ok) return mensajeError.classList.toggle("escondido", false);
+
+    const resJson = await res.json();
+    if (resJson.redirect) {
+        window.location.href = resJson.redirect;
+    }
+})
