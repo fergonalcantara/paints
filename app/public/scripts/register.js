ecdc9ca85fd65a323dc70b426098dde0c61c0664
fergonalcantara <fergonalcantara@gmail.com>
2025-10-14 00:54:53 -0600

    POST - register.js

--- app/public/scripts/register.js
+++ app/public/scripts/register.js
@@ -1,4 +1,27 @@
-document.getElementById("register-form").addEventListener("submit", (e) => {
+const mensajeError = document.getElementsByClassName("error")[0];
+
+document.getElementById("register-form").addEventListener("submit", async (e) => {
     e.preventDefault();
-    console.log(e);
+
+    const form = e.target;
+
+    const body = {
+        name: form.elements.name.value.trim(),
+        email: form.elements.email.value.trim(),
+        user: form.elements.user.value.trim(),
+        password: form.elements.password.value.trim()
+    };
+
+    const res = await fetch("http://localhost:4000/api/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    if (!res.ok) return mensajeError.classList.toggle("escondido", false);
+    const resJson = await res.json();
+    if (resJson.redirect) {
+        window.location.href = resJson.redirect;
+    }
 })
